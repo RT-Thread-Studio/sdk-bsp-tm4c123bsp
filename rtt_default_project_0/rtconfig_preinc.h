bbb4418690b3d8097f2b41d90ddd7ea9a51dc9b2
null <2584456014@qq.com>
2021-01-28 15:03:51 +0800

    init

---
+++ rtt_default_project_0/rtconfig_preinc.h
@@ -0,0 +1,14 @@
+
+#ifndef RTCONFIG_PREINC_H__
+#define RTCONFIG_PREINC_H__
+
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread pre-include file */
+
+#define HAVE_CCONFIG_H
+#define PART_TM4C123GH6PM
+#define RT_USING_NEWLIB
+#define TARGET_IS_TM4C123_RB1
+#define __RTTHREAD__
+
+#endif /*RTCONFIG_PREINC_H__*/
